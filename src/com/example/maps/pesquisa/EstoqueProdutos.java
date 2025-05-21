79f7366d5cf0a66595350f5610cd164e6ab0d5c2
Samuel Erbet <samuelerbet@gmail.com>
2025-05-21 19:11:41 -0300

    completando estoqueProdutos e produtos

--- src/com/example/maps/pesquisa/EstoqueProdutos.java
+++ src/com/example/maps/pesquisa/EstoqueProdutos.java
@@ -9,19 +9,77 @@ public class EstoqueProdutos {
     public EstoqueProdutos() {
         this.produtos = new HashMap<>();
     }
-    public void adicionarProduto(String nome, int quantidade, double preco, int codigo){
-        produtos.put(codigo,new Produto(nome,quantidade,preco,codigo));
+    public void adicionarProduto(String nome, int quantidade, double preco, int codigo ){
+        produtos.put( codigo,new Produto(nome,quantidade,preco ));
     }
     public void exibirProdutos(){
         System.out.println(produtos);
     }
     public void calcularEstoque(){
-        int total =0;
+        int total = 0;
         int count = 0;
-        if (produtos.containsKey(produtos.get(count).getQuantidade())){
-            total+=produtos.get(count).getQuantidade();
-            count+=1;
+        if (!produtos.isEmpty()){
+            for (Produto p : produtos.values()){
+                total+= p.getQuantidade();
+            }
         }
         System.out.println("o total de estoque foi : "+total);
+    }
+
+    public double maisCaro(){
+        double valor =0;
+
+        if (!produtos.isEmpty()) {
+            for (Produto p : produtos.values()){
+                if (valor<p.getPreco()){
+                    valor = p.getPreco();
+                }
+            }
+        }
+            return valor;
+        }
+
+        public double menorPreco() {
+            double valor = 9999999;
+
+            if (!produtos.isEmpty()) {
+                for (Produto p : produtos.values()) {
+                    if (valor > p.getPreco()) {
+                        valor = p.getPreco();
+                    }
+                }
+            }
+            return valor;
+        }
+
+    public String maiorQuantidade(){
+        int valor =0;
+        String produto = null;
+        double total = 0;
+        if (!produtos.isEmpty()) {
+            for (Produto p : produtos.values()){
+                if (valor<p.getQuantidade()){
+                    valor = p.getQuantidade();
+                    produto = p.getNome();
+                    total = p.getQuantidade() * p.getPreco();
+                }
+            }
+        }
+        return "o produto com maior quantidade é : "+produto+" com "+valor+" no estoque"+" custando o lote "+total;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos produtos = new EstoqueProdutos();
+        produtos.adicionarProduto("creme",134,15.75,1);
+        produtos.adicionarProduto("leite",10000,9.5,2);
+        produtos.adicionarProduto("biscoito",10,5.75,3);
+        produtos.adicionarProduto("creme premium",11,150.75,4);
+
+        produtos.exibirProdutos();
+
+        produtos.calcularEstoque();
+        System.out.println("o produto mais caro custa: "+produtos.maisCaro());
+        System.out.println("o produto mais barato custa: "+produtos.menorPreco());
+        System.out.println(produtos.maiorQuantidade());
     }
 }
--- src/com/example/maps/pesquisa/Produto.java
+++ src/com/example/maps/pesquisa/Produto.java
@@ -4,13 +4,11 @@ public class Produto {
     private String nome;
     private int quantidade;
     private double preco;
-    private long codigo;
 
-    public Produto(String nome, int quantidade, double preco, long codigo) {
+    public Produto(String nome, int quantidade, double preco ) {
         this.nome = nome;
         this.quantidade = quantidade;
         this.preco = preco;
-        this.codigo = codigo;
     }
 
     public String getNome() {
@@ -37,11 +35,15 @@ public class Produto {
         this.preco = preco;
     }
 
-    public long getCodigo() {
-        return codigo;
-    }
 
-    public void setCodigo(long codigo) {
-        this.codigo = codigo;
+
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", quantidade=" + quantidade +
+                ", preco=" + preco;
+
     }
 }
